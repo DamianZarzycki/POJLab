a0efaf27b1843af858151ae7bf7cc22ed18fab6a
DamianZarzycki <s16547@pjwstk.edu.pl>
2018-03-05 21:09:14 +0100

    projects

---
+++ Lab/src/Pierwszy/Hellovol3.java
@@ -0,0 +1,15 @@
+package Pierwszy;
+public class Hello {
+
+    public class Main
+    {
+        public static void main (String[] args)
+        {
+            Random rand = new Random();
+            int a = rand.nextInt(6);
+            System.out.println(a);
+        }
+    }
+
+}
+
